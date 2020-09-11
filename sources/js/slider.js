bdf975d31574786fbcf1c51daa3a387c3562a5f0
maxim-maidan <58174171+maxim-maidan@users.noreply.github.com>
2020-09-11 21:14:15 +0300

    Adding new element.

---
+++ sources/js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    fade: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    nextArrow: '<button type="button" class="slick__button slick__button--next">></button>',
+    prevArrow: '<button type="button" class="slick__button slick__button--prev"><</button>',
+});
+
